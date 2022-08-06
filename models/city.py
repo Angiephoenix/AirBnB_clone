7815d869ae49ed86c90ce50b65cee2c897905ed7
Precious Nwaigbo <99683218+Tactical98@users.noreply.github.com>
2022-08-06 23:48:05 +0100

    Create city.py

---
+++ models/city.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Defines city
+"""
+from models.base_model import BaseModel
+
+
+class City(BaseModel):
+    """defines city to look for"""
+    state_id = ""
+    name = ""
+    
