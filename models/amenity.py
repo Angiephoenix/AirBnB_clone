325d8627685f6d1a59aca25e7a0fffb8d6f27c77
Precious Nwaigbo <99683218+Tactical98@users.noreply.github.com>
2022-08-06 23:41:04 +0100

    airbnb task

---
+++ models/amenity.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+Defines amenities
+"""
+from models.base_model import BaseModel
+
+
+class Amenity(BaseModel):
+    """Defines amenities that user can choose from to offer at its place"""
+    name = ""
+    
