3857ca76bef6dc73887735d35b06e8ae176890eb
Angiephoenix <angiephoenix93@gmail.com>
2022-08-09 19:45:43 -0700

    update

--- models/__init__.py
+++ models/__init__.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
-
-"""
-An initializer of the models package
-"""
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
